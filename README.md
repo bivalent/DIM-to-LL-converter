c034daf2f0af34096df68a36ef016ae395467e04
Brent <riva.brent@gmail.com>
2021-08-28 16:50:24 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Read Me
+
+This project is an ugly written python script to convert DIM wishlists (txt) into LittleLight (json) format to review in visual form the DIM wishlist contents.
+
+## To use:
+name the input txt file as... 'input.txt' and run `python3 convert.py`
+
+`output.json` will then write to the project directory, you can then upload that to Little Light's website.
+
+
+## Contributing
+My python is ugly, partially because I use it when hacking a quick script together, and partially because I disagree with some python styling as someone from a C++/Java background.
+
+That being said: Please clean up the code as you wish and would like to! It can use the cleanup / improvements.
+
+My only strong preference is for contributors to use camelCasing vs. the_python_underscoring. If this project ends up such that I'm not the one maintaining it, feel free to change the style to whatever the maintainer(s) prefer most.
+
+
+## Issues
+
+If there's an issue -- post an issue. I'll look to it when I have time. Otherwise, see: Contributing
---
+++ convert.py
@@ -0,0 +1,81 @@
+# convert .txt CSV style file into an LL json
+import json
+
+outputDict = dict()
+rollData = list()
+
+def parseName(line):
+    #// NameIsHere
+    return line[2:].strip()
+
+def parseDescriptionAndTags(line):
+    #//notes: balalahahlalha tags: (pve,mk)
+    sections = line.split(':')
+    # //notes: | desc 'tags' | actual tags
+    desc = sections[1]
+    # trim off the 'tags' part
+    desc = desc[:len(desc) - 4].strip()
+
+    tag_s = sections[2]
+    tag_l = list(map(strip, tag_s.split(',')))
+
+    return desc, tag_l
+
+def parseItemAndPerks(line):
+    l = line.split(':')[1]
+    ip = l.split('&')
+    item = ip[0].split('=')[1]
+    perks = ip[1].split('=')[1].split(',')
+
+    return item, perks
+
+with open('input.txt', 'r') as dimFile:
+    title = dimFile.readline().split(':')[1].strip()
+    desc = dimFile.readline().split(':')[1].strip()
+
+    lines = dimFile.readlines()
+
+    ## item parsing
+    i = 0
+    while i < len(lines):
+        print(lines[i])
+
+        if len(lines[i].strip()) == 0:
+            i += 1
+            continue
+
+        #item metadata
+        itemName = parseName(lines[i].strip())
+        itemDesc, itemTags = parseDescriptionAndTags(lines[i+1].strip())
+        perkColumns = [set(), set(), set(), set()]
+        itemHash = ""
+
+        # now for perk rolls.
+        i = i + 2
+        while len(lines[i].strip()) > 0:
+            print(lines[i])
+            itemHash, perkList = parseItemAndPerks(lines[i].strip())
+
+            for j in range(4):
+                perkColumns[j].add(perkList[j])
+            i = i + 1
+
+        # END perk parsing. save it to the list and prepare for next item or EOF
+        item = dict()
+        item['name'] = itemName
+        item['description'] = itemDesc
+        item['hash'] = int(itemHash)
+        item['plugs'] = list(map(list, perkColumns))
+        item['tags'] = itemTags
+
+        rollData.append(item)
+        i += 1
+        #END Item Parsing
+    # END file/Wishlist parsing
+    outputDict['name'] = title
+    outputDict['description'] = desc
+    outputDict['data'] = rollData
+
+    with open('output.json', 'w') as outfile:
+        outputJson = json.dumps(outputDict)
+        outfile.write(outputJson)
